556a8d20336f5d0bf96d51a2560fc4f2fa01843f
khalid7487 <khalid15-7487@diu.edu.bd>
2022-06-26 20:36:56 +0600

    stack

---
+++ data-structure/stack/stack.js
@@ -0,0 +1,55 @@
+class Stack{
+    constructor(){
+        this.items = [];
+    }
+    
+    //add new element into the list
+    add(element){
+        return this.items.push(element);
+    }
+    
+    //remove element from the list
+    remove(){
+        if(this.items.length > 0){
+            return this.items.pop();
+        }
+    }
+    
+    //view the last element
+    peek(){
+        return this.items[this.items.length -1];
+    }
+    
+    //check if the stack is empty
+    isEmpty(){
+        return this.items.length==0
+    }
+    
+    //the size of the stack
+    size(){
+        return this.items.length
+    }
+    
+    //empty the stack
+    clear(){
+        this.items =[]
+    }
+}
+
+let stack = new Stack();;
+
+stack.add(1);
+stack.add(2);
+stack.add(4);
+stack.add(8);
+console.log(stack.items);
+
+stack.remove();
+console.log(stack.items);
+
+console.log(stack.peek());
+
+console.log(stack.isEmpty())
+console.log(stack.size())
+stack.clear();
+console.log(stack.items);
---
+++ data-structure/stack/stack.md
@@ -0,0 +1,6 @@
+## Stack:
+Stacks are linear data structures that allow operations only on one end, meaning that all basic operations like inertion can only be done at the ends of the structure. This reason is because of the concept of Last in First Out(LIFO).
+
+## Basic operations 
+1. Push - inserts into the stack.
+2. Pop - deletes from the stack.
