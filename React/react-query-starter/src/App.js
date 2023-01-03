6fa2f6bb9ef23cc212c47ff70d0dbd2c26efb46c
khalid7487 <khalid15-7487@diu.edu.bd>
2023-01-03 09:43:19 +0600

    fds

--- React/react-query-starter/src/App.js
+++ React/react-query-starter/src/App.js
@@ -1,40 +1,48 @@
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
+import { QueryClientProvider, QueryClient } from 'react-query'
 import './App.css'
 import { HomePage } from './components/Home.page'
 import { RQSuperHeroesPage } from './components/RQSuperHeroes.page'
 import { SuperHeroesPage } from './components/SuperHeroes.page'
 
+const queryClient = QueryClient();
+
 function App() {
   
   return (
-    <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to='/'>Home</Link>
-            </li>
-            <li>
-              <Link to='/super-heroes'>Traditional Super Heroes</Link>
-            </li>
-            <li>
-              <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
-            </li>
-          </ul>
-        </nav>
-        <Switch>
-          <Route path='/super-heroes'>
-            <SuperHeroesPage />
-          </Route>
-          <Route path='/rq-super-heroes'>
-            <RQSuperHeroesPage />
-          </Route>
-          <Route path='/'>
-            <HomePage />
-          </Route>
-        </Switch>
-      </div>
-    </Router>
+    <QueryClientProvider client={queryClient}>
+   
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to='/'>Home</Link>
+              </li>
+              <li>
+                <Link to='/super-heroes'>Traditional Super Heroes</Link>
+              </li>
+              <li>
+                <Link to='/rq-super-heroes'>RQ Super Heroes</Link>
+              </li>
+            </ul>
+          </nav>
+          <Switch>
+            <Route path='/super-heroes'>
+              <SuperHeroesPage />
+            </Route>
+            <Route path='/rq-super-heroes'>
+              <RQSuperHeroesPage />
+            </Route>
+            <Route path='/'>
+              <HomePage />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+
+    </QueryClientProvider>
+
   )
 }
 
